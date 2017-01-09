12d154a5ce0800a697c6e905fa5b912df43f2c63
Alex Brick <alexbrick@porch.com>
2017-01-09 00:14:42 -0800

    Adding Markdown, some web-mode improvements

--- init.el
+++ init.el
@@ -15,7 +15,6 @@
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 (setq delete-old-versions t)
-(setq version-control t)
 (setq inhibit-startup-screen t)
 (setq-default require-final-newline t)
 
@@ -104,12 +103,23 @@
 (use-package web-mode
   :ensure t
   :config
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))
 
 ;; JSON
 (use-package json-mode
   :ensure t)
+
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 ;; Which Key
 (use-package which-key
@@ -230,11 +240,46 @@
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
+
+;; These are taken from Steve Yegge's .emacs:
+;; https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+ "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
+ (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (if (get-buffer new-name)
+	 (message "A buffer named '%s' already exists!" new-name)
+   (progn
+     (rename-file filename new-name 1)
+     (rename-buffer new-name)
+     (set-visited-file-name new-name)
+     (set-buffer-modified-p nil))))))
+
+(defun move-buffer-file (dir)
+ "Move both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+ (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+    (if (string-match dir "\\(?:/\\|\\\\)$")
+        (substring dir 0 -1) dir))
+   (newname (concat dir "/" name)))
+
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+  (progn
+    (copy-file filename newname 1)
+    (delete-file filename)
+    (set-visited-file-name newname)
+    (set-buffer-modified-p nil)
+    t))))
+
 (set-group-string "f" "Files")
 (evil-leader/set-key
-  "fc" 'dired-do-copy
   "fe" 'visit-emacs-init
-  "fm" 'dired-do-rename
+  "fm" 'move-buffer-file
+  "fr" 'rename-buffer-and-buffer
   )
 
 ;; Git Keys

83e392823b173d7c49f590dbe0c4cce20435f289
Alex Brick <alex@alexbrick.me>
2021-03-05 09:30:06 +0100

    Adding run-command commands for npm packages

--- init.el
+++ init.el
@@ -1170,6 +1170,32 @@
   :hook (typescript-mode . prettier-js-mode)
   )
 
+(defun run-command-recipe-package-json--get-scripts (package-json-file)
+  "Extract NPM scripts from PACKAGE-JSON-FILE."
+  (with-temp-buffer
+    (insert-file-contents package-json-file)
+    (when-let ((script-hash (gethash "scripts" (json-parse-buffer))))
+      (let (scripts '())
+        (maphash (lambda (key _value) (push key scripts)) script-hash)
+        scripts))))
+
+(defun run-command-recipe-package-json ()
+  (when-let* ((project-dir
+               (locate-dominating-file default-directory "package.json"))
+              (scripts
+               (run-command-recipe-package-json--get-scripts (concat project-dir "package.json")))
+              (script-runner
+               (if (file-exists-p (concat project-dir "yarn.lock")) "yarn" "npm")))
+    (mapcar (lambda (script)
+              (list :command-name script
+                    :command-line (concat script-runner " run " script)
+                    :display script
+                    :working-dir project-dir))
+            scripts)))
+
+
+(add-to-list 'run-command-recipes #'run-command-recipe-package-json)
+
 ;; XML
 
 (use-package nxml-mode

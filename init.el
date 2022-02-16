c63c513423a9ef72647be86860a78fab4bdd084d
Alex Brick <alex@alexbrick.me>
2022-02-16 09:49:21 +0100

    Removing Windows support

--- init.el
+++ init.el
@@ -1458,41 +1458,6 @@ FACE, FRAME, and ARGS as in `set-face-attribute'."
   (add-to-list 'edit-server-url-major-mode-alist '("wikifiniens" . mediawiki-mode))
   )
 
-;; Windows Support
-
-;; Sets your shell to use cygwin's bash, if Emacs finds it's running
-;; under Windows and c:\cygwin exists. Assumes that C:\cygwin\bin is
-;; not already in your Windows Path (it generally should not be).
-;;
-(let* ((cygwin-root "c:/cygwin64")
-       (cygwin-bin (concat cygwin-root "/bin")))
-  (when (and (eq 'windows-nt system-type)
-             (file-readable-p cygwin-root))
-    
-    (setq exec-path (cons cygwin-bin exec-path))
-    (setenv "PATH" (concat cygwin-bin ";" (getenv "PATH")))
-    
-    ;; By default use the Windows HOME.
-    ;; Otherwise, uncomment below to set a HOME
-    ;;      (setenv "HOME" (concat cygwin-root "/home/eric"))
-    
-    ;; NT-emacs assumes a Windows shell. Change to bash.
-    (setq shell-file-name "bash")
-    (setenv "SHELL" shell-file-name)
-    (setq explicit-shell-file-name shell-file-name)
-    
-    ;; This removes unsightly ^M characters that would otherwise
-    ;; appear in the output of java applications.
-    (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
-
-(defun dos2unix ()
-  "Convert the line endings in this file from Windows to UNIX."
-  (interactive)
-  (while (search-forward "
-" nil t)
-    (replace-match nil nil t))
-  )
-
 ;; Time
 (use-package time
   :config
